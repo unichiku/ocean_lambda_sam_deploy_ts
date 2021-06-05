80ba5fe6849ef014ac4432f0b617a96c1107f969
uni-ezobafun <fukaya.s941109@gmail.com>
2021-06-05 16:38:44 +0900

    [add] buildspec for aws code build

---
+++ aws_code_build/lambda_sam_deploy_ts.sh
@@ -0,0 +1,14 @@
+npm i
+npm run build-dev
+
+aws cloudformation package \
+  --template-file sam/dev/template.yaml \
+  --s3-bucket uni-cloudformation-dev \
+  --s3-prefix sam/lambda_sam_deploy_ts/dev/package \
+  --output-template-file packaged.yaml
+
+aws cloudformation deploy \
+  --template-file packaged.yaml \
+  --s3-bucket uni-cloudformation-dev \
+  --s3-prefix sam/lambda_sam_deploy_ts/dev/package \
+  --stack-name lambda-sam-deploy-ts
