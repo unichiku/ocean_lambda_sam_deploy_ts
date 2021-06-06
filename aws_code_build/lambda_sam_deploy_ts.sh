3b28740eafe0ad64bb97540232f92df61641a400
uni-ezobafun <fukaya.s941109@gmail.com>
2021-06-06 16:14:35 +0900

    [update] dev/prd deploy shell

--- aws_code_build/lambda_sam_deploy_ts.sh
+++ aws_code_build/lambda_sam_deploy_ts.sh
@@ -1,10 +1,19 @@
+#!/bin/sh
+
+if [ $# < 1 ]; then
+  echo "[ERROR!] one or more arguments are required"
+  exit -1
+fi
+
+STAGE=$1
+
 npm i
-npm run build-dev
+npm run build-${STAGE}
 
 aws cloudformation package \
-  --template-file sam/dev/template.yaml \
-  --s3-bucket uni-cloudformation-dev \
-  --s3-prefix sam/lambda_sam_deploy_ts/dev/package \
+  --template-file sam/${STAGE}/template.yaml \
+  --s3-bucket uni-cloudformation-artifact-${STAGE} \
+  --s3-prefix sam/lambda_sam_deploy_ts/${STAGE}/package \
   --output-template-file packaged.yaml
 
 aws cloudformation deploy \
